491a6a478f422fd2e54847097b65d01bc61b2ccc
946818508@qq.com <scl199303>
2018-11-08 16:56:48 +0800

    *

--- www/blog/public/index.php
+++ www/blog/public/index.php
@@ -4,21 +4,21 @@ echo '<p>php环境搭建成功</p>';
 try {
     new PDO('mydb', 'root', '1234567');
     echo '<p>mysql服务已开启</p>';
-}catch (Exception $e){
+} catch (Exception $e) {
     echo '<p>mysql扩展未装</p>';
-}
+} finally {
 
-try {
-    $redis = new Redis();
-    $res = @$redis->connect('myredis', 6379);
-    if(!$res){
-        throw new Exception('<p>redis扩展未装</p>');
+    try {
+        $redis = new Redis();
+        $res = @$redis->connect('myredis', 6379);
+        if (!$res) {
+            throw new Exception('<p>redis扩展未装</p>');
+        }
+        $redis->close();
+        echo '<p>redis服务已开启</p>';
+    } catch (Exception $e) {
+        echo '<p>redis扩展未装</p>';
+    } finally {
+        var_dump(phpinfo());
     }
-    $redis->close();
-    echo '<p>redis服务已开启</p>';
-}catch (Exception $e){
-    echo '<p>redis扩展未装</p>';
 }
-
-
-var_dump(phpinfo());
