2f20eb819c4391903fa09c9004b95d71625d494c
ydtg199303 <946818508@qq.com>
2019-05-18 19:35:22 +0800

    mysqldump

---
+++ mysql/baskup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. /etc/profile
+
+DIR=/var/lib/mysql/backup
+if [ ! -e $DIR ]
+then
+/bin/mkdir -p $DIR
+fi
+NOWDATE=$(date +%Y%m%d)
+/usr/bin/mysqldump --all-databases -uroot -p "123456" | gzip > "$DIR/data_$NOWDATE.sql.gz"
+
+/usr/bin/find $DIR -mtime +7 -name "data_[1-9]*.sql.gz" -exec rm -rf {} \;
