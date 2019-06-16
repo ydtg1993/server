ee5061524e129a7150b2f2e3008ff0d12aa47eb1
ydtg199303 <946818508@qq.com>
2019-06-16 21:49:36 +0800

    *

--- mysqlback/baskup.sh
+++ mysqlback/baskup.sh
@@ -7,7 +7,7 @@ if [ ! -e $DIR ]
 then
 /bin/mkdir -p $DIR
 fi
-NOWDATE=$(date +%Y%m%d)
+NOWDATE=$(date +%Y%m%d%H%M%S)
 /usr/bin/mysqldump --all-databases -uroot -p "123456" | gzip > "$DIR/data_$NOWDATE.sql.gz"
 
 /usr/bin/find $DIR -mtime +7 -name "data_[1-9]*.sql.gz" -exec rm -rf {} \;
