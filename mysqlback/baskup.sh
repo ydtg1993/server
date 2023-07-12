d30f396714d7d1bf3f558e9eff55d553e35f0911
hikki <hikki@qqc88.net>
2023-07-12 15:56:43 +0800

    v1

--- mysqlback/baskup.sh
+++ mysqlback/baskup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-. /etc/profile
-
 DIR=/var/lib/mysql/backup
 if [ ! -e $DIR ]
 then
 /bin/mkdir -p $DIR
 fi
 NOWDATE=$(date +%Y%m%d%H%M%S)
-#mysql_config_editor set --login-path=mysqldump --host=localhost --user=root --password
-/usr/bin/mysqldump --login-path=mysqldump --all-databases > "/var/lib/mysql/backup/data_$NOWDATE.sql"
+/usr/bin/mysqldump --defaults-file=/etc/mysql/conf.d/mysqldump.cnf --all-databases --quick | gzip > "/var/lib/mysql/backup/data_$NOWDATE.sql.gz"
 
 /usr/bin/find $DIR -mtime +7 -name "data_[1-9]*.sql.gz" -exec rm -rf {} \;
