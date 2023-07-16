bac39f4c07b648b0c4e1762003005f16441f7c4c
ydtg1993 <946818508@qq.com>
2023-07-16 23:25:07 +0800

    v1.3

--- compose.dockerfiles/php/conf.d.backup.sh
+++ compose.dockerfiles/php/conf.d.backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sleep 10
+
 while true; do
   # 检查目录是否存在文件
   if [ "$(ls -A /usr/local/etc/php/conf.d.backup 2>/dev/null)" ] || [ ! "$(ls -A /usr/local/etc/php/conf.d 2>/dev/null)" ]; then
@@ -7,8 +7,7 @@ while true; do
     cp -r /usr/local/etc/php/conf.d.backup/* /usr/local/etc/php/conf.d
     break
   else
-    # 等待 10 秒后继续循环
-    sleep 10
+    sleep 3
   fi
 done
 tail -f /dev/null
